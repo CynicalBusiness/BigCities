69c4c8f1154f6c29fdcd548770c8111c6bbc42c8
Matthew Struble <Matt@Mayateck.net>
2013-09-02 08:09:11 -0700

    Base event handler.

---
+++ src/net/mayateck/BigCities/EventHandler.java
@@ -0,0 +1,11 @@
+package net.mayateck.BigCities;
+
+import org.bukkit.event.Listener;
+
+public class EventHandler implements Listener{
+	private BigCities plugin;
+	public EventHandler(BigCities plugin) {
+		this.plugin = plugin;
+	}
+
+}
